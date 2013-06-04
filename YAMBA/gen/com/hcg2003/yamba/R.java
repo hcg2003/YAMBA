e31a8c4d93e8b73e39a93b4de650a714cb0b8875
hcg2003 <hcg2003@gmail.com>
2013-06-04 11:12:04 +0800

    Text Watcher

--- YAMBA/gen/com/hcg2003/yamba/R.java
+++ YAMBA/gen/com/hcg2003/yamba/R.java
@@ -24,7 +24,6 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
         public static final int btnUpdate=0x7f090003;
         public static final int evTweet=0x7f090002;
         public static final int itemPrefs=0x7f090004;
@@ -37,7 +36,6 @@ public final class R {
     }
     public static final class menu {
         public static final int menu=0x7f080000;
-        public static final int yamb=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
--- YAMBA/src/com/hcg2003/yamba/YAMBA.java
+++ YAMBA/src/com/hcg2003/yamba/YAMBA.java
@@ -1,5 +1,7 @@
 package com.hcg2003.yamba;
 
+import org.w3c.dom.Text;
+
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.os.AsyncTask;
@@ -11,6 +13,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.graphics.Color;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -58,26 +61,17 @@ public class YAMBA extends Activity
 	
 	private void findViews()
 	{
+		//Setup text counter
 		tvCount = (TextView)findViewById(R.id.tvCount);
+		tvCount.setText(Integer.toString(140));
+		tvCount.setTextColor(Color.GREEN);
+		
 		etTweet = (EditText)findViewById(R.id.evTweet);
 		btnUpadte = (Button)findViewById(R.id.btnUpdate);
 		//Setup Preferences
 		shPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 	}
 	
-	private Twitter getTwitter()
-	{
-		if(twitter == null)
-		{
-			String username= shPrefs.getString("username","");
-			String password= shPrefs.getString("password", "");
-			String apiRoot= shPrefs.getString("apiRoot", "http://yamba.marakana.com/api");
-			twitter = new Twitter(username, password);
-			twitter.setAPIRootUrl(apiRoot);			
-		}
-		return twitter;
-	}
-
 	private void listeners()
 	{
 		btnUpadte.setOnClickListener(new OnClickListener()
@@ -87,11 +81,11 @@ public class YAMBA extends Activity
 			{
 //				twitter.setStatus(etTweet.getText().toString());
 				new PostToTwitter().execute(etTweet.getText().toString());
-				Log.d(TAG, "onClicked");
 			}
 		});
 		
-		tvCount.addTextChangedListener(new TextWatcher()
+		//Text Counter Listener
+		etTweet.addTextChangedListener(new TextWatcher()
 		{
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count){}
@@ -102,7 +96,17 @@ public class YAMBA extends Activity
 			@Override
 			public void afterTextChanged(Editable s)
 			{
-				int count = 140 - s.length();		
+				int count = 140 - s.length();
+				tvCount.setText(Integer.toString(count));
+				tvCount.setTextColor(Color.GREEN);
+				if(count < 10)
+				{
+					tvCount.setTextColor(Color.YELLOW);
+				}
+				if(count < 0)
+				{
+					tvCount.setTextColor(Color.RED);
+				}				
 			}
 		});
 		
@@ -117,6 +121,19 @@ public class YAMBA extends Activity
 		});
 	}
 
+	private Twitter getTwitter()
+	{
+		if(twitter == null)
+		{
+			String username= shPrefs.getString("username","");
+			String password= shPrefs.getString("password", "");
+			String apiRoot= shPrefs.getString("apiRoot", "http://yamba.marakana.com/api");
+			twitter = new Twitter(username, password);
+			twitter.setAPIRootUrl(apiRoot);			
+		}
+		return twitter;
+	}
+	
 	class PostToTwitter extends AsyncTask<String, Integer, String>{
 
 		@Override
