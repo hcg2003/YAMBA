2cb81af590b94c4a8fb58c7e1a9524893162952a
hcg2003 <hcg2003@gmail.com>
2013-06-04 10:49:56 +0800

    PreferenceActivity SharedPreferences Menu

---
+++ YAMBA/gen/com/hcg2003/yamba/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hcg2003.yamba;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- YAMBA/src/com/hcg2003/yamba/YAMBA.java
+++ YAMBA/src/com/hcg2003/yamba/YAMBA.java
@@ -5,23 +5,33 @@ import winterwell.jtwitter.TwitterException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class YAMBA extends Activity
 {
 	private static final String TAG = "StatusActivity";
+	TextView tvCount;
 	EditText etTweet;
 	Button btnUpadte;
 	Twitter twitter;
 	ProgressDialog pd;
+	SharedPreferences shPrefs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -42,14 +52,34 @@ public class YAMBA extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_yamba);
 		findViews();
+		listeners();
+		getTwitter();
 	}
 	
 	private void findViews()
 	{
+		tvCount = (TextView)findViewById(R.id.tvCount);
 		etTweet = (EditText)findViewById(R.id.evTweet);
 		btnUpadte = (Button)findViewById(R.id.btnUpdate);
-		twitter = new Twitter("student", "password");
-		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+		//Setup Preferences
+		shPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+	}
+	
+	private Twitter getTwitter()
+	{
+		if(twitter == null)
+		{
+			String username= shPrefs.getString("username","");
+			String password= shPrefs.getString("password", "");
+			String apiRoot= shPrefs.getString("apiRoot", "http://yamba.marakana.com/api");
+			twitter = new Twitter(username, password);
+			twitter.setAPIRootUrl(apiRoot);			
+		}
+		return twitter;
+	}
+
+	private void listeners()
+	{
 		btnUpadte.setOnClickListener(new OnClickListener()
 		{
 			@Override
@@ -60,8 +90,32 @@ public class YAMBA extends Activity
 				Log.d(TAG, "onClicked");
 			}
 		});
+		
+		tvCount.addTextChangedListener(new TextWatcher()
+		{
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count){}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+			
+			@Override
+			public void afterTextChanged(Editable s)
+			{
+				int count = 140 - s.length();		
+			}
+		});
+		
+		//SharedPreferences Listener
+		shPrefs.registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener()
+		{
+			@Override
+			public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+			{
+				twitter = null;			
+			}
+		});
 	}
-
 
 	class PostToTwitter extends AsyncTask<String, Integer, String>{
 
@@ -89,7 +143,6 @@ public class YAMBA extends Activity
 		{
 			Toast.makeText(YAMBA.this, result, Toast.LENGTH_SHORT).show();
 			pd.dismiss();
-//			super.onPostExecute(result);
 		}
 		
 	}
@@ -97,9 +150,19 @@ public class YAMBA extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.yamb, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.itemPrefs:
+			startActivity(new Intent(this, PrefsActivity.class));
+			break;
+		}
+		return true;
+	}
 }
